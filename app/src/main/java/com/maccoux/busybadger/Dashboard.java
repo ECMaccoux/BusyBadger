3d6c3f166cf8e4ca32aa1581faccfc226ba43f66
Jimmy <jimgriff8@gmail.com>
2020-03-13 19:06:14 -0500

    created layouts and classes for upcoming/dashboard screen

---
+++ app/src/main/java/com/maccoux/busybadger/Dashboard.java
@@ -0,0 +1,18 @@
+package com.maccoux.busybadger;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+
+
+    }
+
+}
