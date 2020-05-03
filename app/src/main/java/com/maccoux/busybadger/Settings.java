670c42b312db14406927db35bc5d84fc91b88cf2
JaredGlaub <jglaub@wisc.edu>
2020-05-02 20:54:52 -0500

    Settings Dialog Boxes

--- app/src/main/java/com/maccoux/busybadger/Settings.java
+++ app/src/main/java/com/maccoux/busybadger/Settings.java
@@ -1,5 +1,7 @@
 package com.maccoux.busybadger;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
@@ -7,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -30,14 +33,36 @@ public class Settings extends AppCompatActivity {
     }
 
     public void onEditEventButton(View v) {
-        Toast.makeText(this, "Edit events button clicked!", Toast.LENGTH_SHORT).show();
+        String[] multiChoiceItems = getResources().getStringArray(R.array.eventListArray);
+        final boolean[] checkedItems = {false, false};
+        new AlertDialog.Builder(this, R.style.MyDialogTheme)
+                .setTitle(R.string.EditEventList)
+                .setMultiChoiceItems(multiChoiceItems, checkedItems, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int index, boolean isChecked) {
+                        Log.d("MainActivity", "clicked item index is " + index);
+                    }
+                })
+                .setPositiveButton("Ok", null)
+                .setNegativeButton("Cancel", null)
+                .show();
     }
     public void onEditListButton(View v) {
-        Toast.makeText(this, "Edit class list button clicked!", Toast.LENGTH_SHORT).show();
+        String[] multiChoiceItems = getResources().getStringArray(R.array.classListArray);
+        final boolean[] checkedItems = {false, false};
+        new AlertDialog.Builder(this, R.style.MyDialogTheme)
+                .setTitle(R.string.EditClassList)
+                .setMultiChoiceItems(multiChoiceItems, checkedItems, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int index, boolean isChecked) {
+                        Log.d("MainActivity", "clicked item index is " + index);
+                    }
+                })
+                .setPositiveButton("Ok", null)
+                .setNegativeButton("Cancel", null)
+                .show();
     }
-//    public void onExportButton(View v) {
-//        Toast.makeText(this, "Google Calendar export button clicked!", Toast.LENGTH_SHORT).show();
-//    }
+
     public void onResetButton(View v) {
         Toast.makeText(this, "Reset button clicked!", Toast.LENGTH_SHORT).show();
     }
